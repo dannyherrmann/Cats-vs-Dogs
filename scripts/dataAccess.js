73cee2aceb7a540d3acc01314613705179631782
Danny Herrmann <dannyherrmann91@gmail.com>
2022-10-03 21:32:13 -0500

    adding api links and boilerplate html

---
+++ scripts/dataAccess.js
@@ -0,0 +1,7 @@
+const applicationState = {
+    cats: [],
+    dogs: []
+}
+
+const catAPI = "https://api.thecatapi.com/v1/images/search"
+const dogAPI = "https://api.thedogapi.com/v1/images/search"
