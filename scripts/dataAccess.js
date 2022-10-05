e3c1254b219af5a8e9e3f08699ffdc7d63e316a1
Erica Clayton <erica.baertsch@gmail.com>
2022-10-04 19:14:36 -0500

    added cat fetch call

--- scripts/dataAccess.js
+++ scripts/dataAccess.js
@@ -1,7 +1,17 @@
 const applicationState = {
     cats: [],
     dogs: []
-}
+};
 
 const catAPI = "https://api.thecatapi.com/v1/images/search"
 const dogAPI = "https://api.thedogapi.com/v1/images/search"
+
+export const fetchCatAPI = async () => {
+    const dataFetch = await fetch(`${catAPI}`)
+    const jsonData = await dataFetch.json()
+    applicationState.cats = jsonData;
+};
+
+export const getCats = () => {
+    return applicationState.cats.map((x) => ({...x}))
+};
---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import { fetchCatAPI } from "./dataAccess.js";
+
+const mainContainer = document.querySelector("#container")
+
+// const render = async () => {
+//     await fetchCatAPI ();
+//     mainContainer.innerHTML = 
+// }
